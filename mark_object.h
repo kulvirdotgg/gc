2f4c82e287cdfc1fef423a01c4e63cdcbba1f0d6
Kulvir Singh <kulvirs734@gmail.com>
2025-04-12 19:53:58 +0530

    made a new object for mark and sweep gc

---
+++ mark_object.h
@@ -0,0 +1,45 @@
+#pragma once
+#include "stack.h"
+#include "vm.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// early declaration so that we can reference object_t before its definition.
+// its named mark_t because it will used by mark and sweep algorithm
+typedef struct Object mark_t;
+
+typedef enum ObjectType { INT, FLOAT, STRING, ARRAY, VECTOR3 } mark_type_t;
+
+typedef struct Array {
+  size_t capacity;
+  mark_t **data;
+} mark_array_t;
+
+// actual vector not cpp vector!!
+typedef struct Vector {
+  mark_t *x;
+  mark_t *y;
+  mark_t *z;
+} mark_vector_t;
+
+typedef union ObjectData {
+  int v_int;
+  float v_float;
+  char *v_string;
+  mark_array_t v_array;
+  mark_vector_t v_vector3;
+} mark_data_t;
+
+typedef struct Object {
+  bool isMarked;
+
+  mark_type_t type;
+  mark_data_t data;
+} mark_t;
+
+mark_t *new_int(vm_t *vm, int value);
+mark_t *new_float(vm_t *vm, float value);
+mark_t *new_string(vm_t *vm, char *value);
+mark_t *new_vector3(vm_t *vm, mark_t *x, mark_t *y, mark_t *z);
+mark_t *new_array(vm_t *vm, size_t capacity);
